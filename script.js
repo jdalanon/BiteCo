cb504d439c0395fcbfac529af0f28eacb14ba3a6
jsurban <fdc.julz@gmail.com>
2026-07-16 21:59:39 +0800

    Modified font color

--- script.js
+++ script.js
@@ -19,23 +19,13 @@ document
 
 });
 
-// Scroll Animation
-
-const observer = new IntersectionObserver((entries)=>{
-
-    entries.forEach(entry=>{
-
-        if(entry.isIntersecting){
-
-            entry.target.classList.add("show");
-
-        }
-
-    });
-
-},{
-    threshold:0.2
-});
-
 document.querySelectorAll(".card,.about-content,.contact form")
 .forEach(el=>observer.observe(el));
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
