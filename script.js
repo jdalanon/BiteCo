2efeeeaa1d518680b3b862e4ca225943e384d00d
jsurban <fdc.julz@gmail.com>
2026-07-16 16:01:23 +0800

    Modified Landing page

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
+// Mobile Menu
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
 
-  function toggleMenu() {
-    document.getElementById("menu").classList.toggle("show");
-  };
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Contact Form
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Thank you! Your message has been sent.");
+
+    this.reset();
+
+});
+
+// Scroll Animation
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+document.querySelectorAll(".card,.about-content,.contact form")
+.forEach(el=>observer.observe(el));
