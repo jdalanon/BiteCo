42955fc96feb26eafe3bb8352774fc1046ed50e7
jsurban <fdc.julz@gmail.com>
2026-07-17 11:46:59 +0800

    Added Cart

--- script.js
+++ script.js
@@ -6,31 +6,6 @@ menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
-// Contact Form
-document
-.getElementById("contactForm")
-.addEventListener("submit", function(e){
-
-    e.preventDefault();
-
-    alert("Thank you! Your message has been sent.");
-
-    this.reset();
-
-});
-
-// Menu Redirection 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-document.querySelectorAll(".card, .about-content, .contact form")
-    .forEach(el => observer.observe(el));
-
 // Cart 
 let cart = [];
 
@@ -97,3 +72,28 @@ function checkout(){
     updateCart();
 
 }
+
+// Contact Form
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Thank you! Your message has been sent.");
+
+    this.reset();
+
+});
+
+// Menu Redirection 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".card, .about-content, .contact form")
+    .forEach(el => observer.observe(el));
